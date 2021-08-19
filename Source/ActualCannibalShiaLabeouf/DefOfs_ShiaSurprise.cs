50f1a5ad1b2acca3f77525dfc105ae9845b5c501
emipa606 <emil.offermo@litet.net>
2021-08-19 13:15:40 +0200

    Added end-of-life message

--- Source/ActualCannibalShiaLabeouf/DefOfs_ShiaSurprise.cs
+++ Source/ActualCannibalShiaLabeouf/DefOfs_ShiaSurprise.cs
@@ -7,20 +7,21 @@ namespace ActualCannibalShiaLabeouf
     public static class DefOfs_ShiaSurprise
     {
         public static FactionDef HollywoodCannibals;
+
         //public static FactionDef SpacerHostile;
         public static PawnKindDef ShiaKind;
         public static BodyTypeDef Male;
         public static PawnsArrivalModeDef EdgeWalkIn;
         public static LetterDef ThreatSmall;
         public static TaleDef Raid;
-
-        
     }
 
     public static class Consts_ShiaSurprise
     {
         public const string path = "Things/Pawn/Humanlike/Heads/Male/Male_Narrow_Wide";
         public const string letterLabel = "Shia Surprise!";
-        public const string letterText = "You're walking in the woods. There's no one around and your phone is dead. Out of the corner of your eye, you spot him: Shia Labeouf!";
+
+        public const string letterText =
+            "You're walking in the woods. There's no one around and your phone is dead. Out of the corner of your eye, you spot him: Shia Labeouf!";
     }
 }
